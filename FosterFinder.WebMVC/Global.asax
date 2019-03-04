66c5c6e33e691179e1979c475ef5a2e8cd2863f2
Laura Garcia <lgarcia223@gmail.com>
2019-03-04 16:57:28 -0500

    Add project files.

---
+++ FosterFinder.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FosterFinder.WebMVC.MvcApplication" Language="C#" %>
---
+++ FosterFinder.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FosterFinder.WebMVC.Startup))]
+namespace FosterFinder.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
