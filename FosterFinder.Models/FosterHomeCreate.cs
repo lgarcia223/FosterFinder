c03ee9b3b91fc2e4cb58700ef19c65338ae1f21c
Laura Garcia <lgarcia223@gmail.com>
2019-03-05 16:54:28 -0500

    FosterHomeCreate

--- FosterFinder.Models/FosterHomeCreate.cs
+++ FosterFinder.Models/FosterHomeCreate.cs
@@ -8,20 +8,30 @@ using System.Threading.Tasks;
 
 namespace FosterFinder.Models
 {
-    public class FosterHomeListItem
+    public class FosterHomeCreate
     {
-        [Display(Name ="Foster Family Name")]
+        [Required]
+        [Display(Name = "Foster Family Name")]
         public string FamilyName { get; set; }
+        [Required]
         [Display(Name = "Openings")]
+        [Range(0, 5, ErrorMessage = "Please choose a number between 0 and 5")]
         public int OpenBeds { get; set; }
+        [Required]
         [Display(Name = "Gender Preference")]
         public GenderPref genderpref { get; set; }
+        [Required]
         [Display(Name = "Minimum Age Preference")]
+        [Range(0, 18, ErrorMessage = "Please choose a number between 0 and 18")]
         public double AgePrefMin { get; set; }
+        [Required]
         [Display(Name = "Maximum Age Preference")]
+        [Range(0, 18, ErrorMessage = "Please choose a number between 0 and 18")]
         public double AgePrefMax { get; set; }
+        [Required]
         [Display(Name = "School District")]
         public string SchoolDistrict { get; set; }
+        [MaxLength(200, ErrorMessage = "There are too many characters in this field.")]
         public string Comments { get; set; }
 
         public DateTimeOffset ModifiedUtc { get; set; }
--- FosterFinder.WebMVC/Controllers/FosterHomeController.cs
+++ FosterFinder.WebMVC/Controllers/FosterHomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FosterFinder.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,28 @@ using System.Web.Mvc;
 
 namespace FosterFinder.WebMVC.Controllers
 {
+    [Authorize]
     public class FosterHomeController : Controller
     {
         // GET: FosterHome
         public ActionResult FosterHomeIndex()
         {
+            var model = new FosterHomeListItem[0];
+            return View(model);
+        }
+        public ActionResult FosterHomeCreate()
+        {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FosterHomeCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
